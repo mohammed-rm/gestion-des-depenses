912a39bea8bc8a2ce86e8bc697707f75e92d5732
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-26 12:28:27 +0200

    .

---
+++ ExpensesApp/src/launching/Main.java
@@ -0,0 +1,11 @@
+package launching;
+
+import view.Appview;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Appview();
+	}
+
+}
--- ExpensesApp/src/view/Appview.java
+++ ExpensesApp/src/view/Appview.java
@@ -1,5 +1,37 @@
 package view;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.WindowConstants;
+
 public class Appview {
 
+	public Appview() {
+		/* FRAME */
+		JFrame frame = new JFrame();
+		frame.setTitle("Expense Tracking Application™");
+		frame.setResizable(false);
+		frame.setSize(800, 500);
+		
+		/* TAB */
+		JTabbedPane tab = new JTabbedPane();
+		
+		/* PANELS */
+		JPanel homePanel = new JPanel();
+		JPanel historyPanel = new JPanel();
+		JPanel settingsPanel = new JPanel();
+		
+		/* ADD COMPONENTS */
+		tab.add(homePanel, "Home"); 
+		tab.add(historyPanel, "History");
+		tab.add(settingsPanel, "Settings");
+		
+		frame.add(tab);
+		
+		/* END FRAME */
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
