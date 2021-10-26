7404a09dc49c7014e3e8a08c0c38920eda4081f8
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-26 12:44:41 +0200

    .

--- ExpensesApp/src/view/Appview.java
+++ ExpensesApp/src/view/Appview.java
@@ -1,6 +1,9 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
@@ -12,7 +15,7 @@ public class Appview {
 		JFrame frame = new JFrame();
 		frame.setTitle("Expense Tracking Application™");
 		frame.setResizable(false);
-		frame.setSize(800, 500);
+		frame.setSize(500, 500);
 		
 		/* TAB */
 		JTabbedPane tab = new JTabbedPane();
@@ -21,11 +24,22 @@ public class Appview {
 		JPanel homePanel = new JPanel();
 		JPanel historyPanel = new JPanel();
 		JPanel settingsPanel = new JPanel();
+		JPanel aboutPanal = new JPanel();
 		
+		/* HOME MENU */
+		JMenuBar bar = new JMenuBar();
+		JMenu menu = new JMenu("+");
+		JMenuItem size = new JMenuItem("New Expenses");
+			
 		/* ADD COMPONENTS */
 		tab.add(homePanel, "Home"); 
 		tab.add(historyPanel, "History");
 		tab.add(settingsPanel, "Settings");
+		tab.add(aboutPanal, "About");
+		
+		menu.add(size);
+		homePanel.add(bar);
+		bar.add(menu);
 		
 		frame.add(tab);
 		
